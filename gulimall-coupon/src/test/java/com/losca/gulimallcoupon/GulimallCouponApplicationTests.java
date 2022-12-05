bf84d34bd454e1e229b86b9c574ba7449e1d58d3
losca <783840358@qq.com>
2022-12-05 18:17:31 +0800

    初始化项目

---
+++ gulimall-coupon/src/test/java/com/losca/gulimallcoupon/GulimallCouponApplicationTests.java
@@ -0,0 +1,13 @@
+package com.losca.gulimallcoupon;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GulimallCouponApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
---
+++ gulimall-ware/src/test/java/com/losca/gulimallware/GulimallWareApplicationTests.java
@@ -0,0 +1,13 @@
+package com.losca.gulimallware;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GulimallWareApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
