1a24c00530ceb0f19554530be83f714e67ce7606
losca <783840358@qq.com>
2023-01-10 12:22:18 +0800

    商品首页 二级、三级分类完成

--- gulimall-product/src/main/java/com/losca/gulimall/product/service/CategoryService.java
+++ gulimall-product/src/main/java/com/losca/gulimall/product/service/CategoryService.java
@@ -4,6 +4,7 @@ import com.baomidou.mybatisplus.extension.service.IService;
 import com.losca.common.utils.PageUtils;
 import com.losca.common.utils.R;
 import com.losca.gulimall.product.entity.CategoryEntity;
+import com.losca.gulimall.product.vo.Catelog2Vo;
 
 import java.util.List;
 import java.util.Map;
@@ -26,5 +27,7 @@ public interface CategoryService extends IService<CategoryEntity> {
     Long[] findCatelogPath(Long catelogId);
 
     List<CategoryEntity> getLevel1Categorys();
+
+    Map<String, List<Catelog2Vo>> getCatalogJson();
 }
 
--- gulimall-product/src/main/java/com/losca/gulimall/product/web/IndexController.java
+++ gulimall-product/src/main/java/com/losca/gulimall/product/web/IndexController.java
@@ -2,12 +2,15 @@ package com.losca.gulimall.product.web;
 
 import com.losca.gulimall.product.entity.CategoryEntity;
 import com.losca.gulimall.product.service.CategoryService;
+import com.losca.gulimall.product.vo.Catelog2Vo;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.ModelMap;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
 
 import java.util.List;
+import java.util.Map;
 
 @Controller
 public class IndexController {
@@ -19,5 +22,16 @@ public class IndexController {
         List<CategoryEntity> categorys = categoryService.getLevel1Categorys();
         map.addAttribute("categories", categorys);
         return "index";
+    }
+
+    //index/json/catalog.json
+    @GetMapping(value = "/index/catalog.json")
+    @ResponseBody
+    public Map<String, List<Catelog2Vo>> getCatalogJson() {
+
+        Map<String, List<Catelog2Vo>> catalogJson = categoryService.getCatalogJson();
+
+        return catalogJson;
+
     }
 }
